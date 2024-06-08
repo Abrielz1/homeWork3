1be094b126c945713fb8bb62c4b07cbc8a9eb0b0
Abriel <abrielz11@gmail.com>
2024-06-08 20:21:12 +0300

    добавил задание 3

--- src/main/java/org/example/part3/Master.java
+++ src/main/java/org/example/part3/Master.java
@@ -1,23 +1,22 @@
 package org.example.part3;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Master {
 
     public static void main(String[] args) {
 
-        Node counter = new Node();
+        var counter = new Node();
 
-        var t1 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T1");
-        var t2 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T2");
-        var t3 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T3");
-
-        t1.start();
-        t2.start();
-        t3.start();
+        Thread t1 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T1");
+        Thread t2 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T2");
+        Thread t3 = new Thread(() -> IntStream.range(0, 10).forEach(i -> counter.log()), "T3");
 
         try {
+
+            Arrays.asList(t1, t2, t3).forEach(Thread::start);
+
             t1.join();
             t2.join();
             t3.join();
